084cce9ffd1054a12730b25507a2527b1f53884b
Vetri <37167788+Vetrivelkv@users.noreply.github.com>
2022-06-30 07:23:27 +0530

    Display_Questions_Incremenet_Operator

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Widget> scoreKeeper = [
+  List<Widget> scoreKeeper = [];
 
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+  int questionNumber=0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -44,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -68,7 +74,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
@@ -87,6 +95,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
